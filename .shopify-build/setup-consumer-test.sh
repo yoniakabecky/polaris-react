c44dfc572f7f7f76f56f7bdd042681883c773330
amrocha <andre.rocha@shopify.com>
2019-06-04 10:05:58 -0700

    debugging ci is so fun

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -2,15 +2,15 @@
 shopt -s extglob
 set -e
 
-ls -l
+ls -la
 mkdir ../tmp/polaris-react
 mv ./build/* .
 mv !(node_modules) ../tmp/polaris-react
 mv ../tmp/polaris-react polaris-react
 git clone ssh://git@github.com/Shopify/$1 --depth 1
-ls -l
+ls -la
 cd $1
-ls -l
+ls -la
 
 yarn upgrade @shopify/polaris@next
 pwd
