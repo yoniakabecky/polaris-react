0ea11745bf2f2aa75bf5c6e3d6d09a1247d984cc
Daniel Leroux <daniel.leroux@shopify.com>
2019-07-10 11:24:24 -0400

    setup

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,102 +1,109 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {ActionListItemDescriptor} from '../../types';
-import {
-  withAppProvider,
-  WithAppProviderProps,
-} from '../../utilities/with-app-provider';
-import {PreferredPosition} from '../PositionedOverlay';
-import {OptionDescriptor} from '../OptionList';
-import Spinner from '../Spinner';
-
-import {TextField, ComboBox} from './components';
-import styles from './Autocomplete.scss';
+import TextField from '../TextField';
+import Popover from '../Popover';
 
 export interface Props {
   /** A unique identifier for the Autocomplete */
   id?: string;
-  /** Collection of options to be listed */
-  options: OptionDescriptor[];
-  /** The selected options */
-  selected: string[];
-  /** The text field component attached to the list of options */
-  textField: React.ReactElement<any>;
-  /** The preferred direction to open the popover */
-  preferredPosition?: PreferredPosition;
-  /** Title of the list of options */
-  listTitle?: string;
-  /** Allow more than one option to be selected */
-  allowMultiple?: boolean;
-  /** An action to render above the list of options */
-  actionBefore?: ActionListItemDescriptor;
-  /** Display loading state */
+  children?: React.ReactNode;
   loading?: boolean;
-  /** Indicates if more results will load dynamically */
-  willLoadMoreResults?: boolean;
-  /** Is rendered when there are no options */
-  emptyState?: React.ReactNode;
-  /** Callback when the selection of options is changed */
-  onSelect(selected: string[]): void;
-  /** Callback when the end of the list is reached */
-  onLoadMoreResults?(): void;
+  allowMultiple?: boolean;
+  onScrolledToBottom?(): void;
+  onSelect?(index: number): void;
 }
 
-type CombinedProps = Props & WithAppProviderProps;
-
-class Autocomplete extends React.PureComponent<CombinedProps, never> {
-  static TextField = TextField;
-  static ComboBox = ComboBox;
-
-  render() {
-    const {
-      id,
-      options,
-      selected,
-      textField,
-      preferredPosition,
-      listTitle,
-      allowMultiple,
-      loading,
-      actionBefore,
-      willLoadMoreResults,
-      emptyState,
-      onSelect,
-      onLoadMoreResults,
-      polaris: {intl},
-    } = this.props;
-
-    const spinnerMarkup = loading ? (
-      <div className={styles.Loading}>
-        <Spinner
-          size="small"
-          accessibilityLabel={intl.translate(
-            'Polaris.Autocomplete.spinnerAccessibilityLabel',
-          )}
-        />
-      </div>
-    ) : null;
-
-    const conditionalOptions = loading && !willLoadMoreResults ? [] : options;
-    const conditionalAction =
-      actionBefore && actionBefore !== [] ? [actionBefore] : undefined;
-
-    return (
-      <ComboBox
-        id={id}
-        options={conditionalOptions}
-        selected={selected}
-        textField={textField}
-        preferredPosition={preferredPosition}
-        listTitle={listTitle}
-        allowMultiple={allowMultiple}
-        contentAfter={spinnerMarkup}
-        actionsBefore={conditionalAction}
-        onSelect={onSelect}
-        onEndReached={onLoadMoreResults}
-        emptyState={emptyState}
-      />
-    );
-  }
+export interface AutoCompleteContextType {
+  allowMultiple?: boolean;
+  loading?: boolean;
+  showEmptyState?: boolean;
+  onScrolledToBottom?(): void;
+  onSelect?(index: number): void;
 }
 
-export default withAppProvider<Props>()(Autocomplete);
+const AutoCompleteContext = React.createContext<AutoCompleteContextType | null>(
+  null,
+);
+
+export default function Autocomplete({
+  id,
+  children,
+  loading,
+  onScrolledToBottom,
+  onSelect,
+  allowMultiple,
+}: Props) {
+  const context = {
+    allowMultiple,
+    loading,
+    onScrolledToBottom,
+    onSelect,
+  };
+
+  const [active, setActive] = useState(false);
+
+  // const autoChildren = React.Children.map(children, (child => {
+  //   if (child.type === TextField) {
+  //     return React.cloneElement(child);
+  //   } else if (child.type === TabList) {
+  //     return React.cloneElement(child, {
+  //       activeIndex: this.state.activeIndex,
+  //       onActivateTab: this.handleTabActivate
+  //     });
+  //   } else {
+  //     return child;
+  //   }
+  // });
+
+  const autoTextField = React.Children && React.Children.toArray(children);
+  console.log(autoTextField);
+  // .filter(
+  //   (child: any) => child.type === TextField,
+  // )[0];
+
+  const autoDataList =
+    React.Children &&
+    React.Children.toArray(children).filter(
+      (child: any) => child.type === DataList,
+    )[0];
+
+  return (
+    <AutoCompleteContext.Provider value={context}>
+      <Popover
+        active={active}
+        activator={autoTextField as React.ReactElement}
+        onClose={() => setActive(false)}
+      >
+        {autoDataList}
+      </Popover>
+      {children}
+    </AutoCompleteContext.Provider>
+  );
+}
+
+function DataList({children}: Props) {
+  return <ul>{children}</ul>;
+}
+
+function Title({children}: Props) {
+  return <li>{children}</li>;
+}
+
+function EmptyState({children}: Props) {
+  return <li>{children}</li>;
+}
+
+function Action({children}: Props) {
+  return <li>{children}</li>;
+}
+
+function Option({children}: Props) {
+  return <li>{children}</li>;
+}
+
+Autocomplete.TextField = TextField;
+Autocomplete.DataList = DataList;
+Autocomplete.Action = Action;
+Autocomplete.Option = Option;
+Autocomplete.Title = Title;
+Autocomplete.EmptyState = EmptyState;
