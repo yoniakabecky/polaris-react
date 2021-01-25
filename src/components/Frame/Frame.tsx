14ed8a721dfdcb8c34a466620349723930e6d7d8
Henry Yi <jita.yi@shopify.com>
2021-01-25 14:50:15 -0500

    Remove #AppFrameMainContent link and update SkipToContent link to target #AppFrameMain instead
    Allow focus to be placed on a focusable element in <main> instead of invisible link

--- src/components/Frame/Frame.tsx
+++ src/components/Frame/Frame.tsx
@@ -65,8 +65,6 @@ const GLOBAL_RIBBON_CUSTOM_PROPERTY = '--global-ribbon-height';
 
 const APP_FRAME_MAIN = 'AppFrameMain';
 
-const APP_FRAME_MAIN_ANCHOR_TARGET = 'AppFrameMainContent';
-
 const APP_FRAME_NAV = 'AppFrameNav';
 const APP_FRAME_TOP_BAR = 'AppFrameTopBar';
 const APP_FRAME_LOADING_BAR = 'AppFrameLoadingBar';
@@ -221,7 +219,7 @@ class FrameInner extends PureComponent<CombinedProps, State> {
 
     const skipTarget = skipToContentTarget?.current
       ? skipToContentTarget.current.id
-      : APP_FRAME_MAIN_ANCHOR_TARGET;
+      : APP_FRAME_MAIN;
 
     const skipMarkup = (
       <div className={skipClassName}>
@@ -262,15 +260,6 @@ class FrameInner extends PureComponent<CombinedProps, State> {
         />
       ) : null;
 
-    const skipToMainContentTarget = skipToContentTarget ? null : (
-      // eslint-disable-next-line jsx-a11y/anchor-is-valid
-      <a
-        id={APP_FRAME_MAIN_ANCHOR_TARGET}
-        ref={this.skipToMainContentTargetNode}
-        tabIndex={-1}
-      />
-    );
-
     const context = {
       showToast: this.showToast,
       hideToast: this.hideToast,
@@ -298,7 +287,6 @@ class FrameInner extends PureComponent<CombinedProps, State> {
             id={APP_FRAME_MAIN}
             data-has-global-ribbon={Boolean(globalRibbon)}
           >
-            {skipToMainContentTarget}
             <div className={styles.Content}>{children}</div>
           </main>
           <ToastManager toastMessages={toastMessages} />
